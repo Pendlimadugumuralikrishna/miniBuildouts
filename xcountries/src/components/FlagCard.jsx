9342af238dff66c65c17d5ecc6792b3f111e9ac7
ImKetan1610 <ketan.makode@gmail.com>
2024-02-10 13:49:48 +0530

    added initial logic

--- xcountries/src/components/FlagCard.jsx
+++ xcountries/src/components/FlagCard.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { data } from "../Data/data";
+import walk from "../assets/Walk.gif";
+import CountryCard from "./CountryCard";
 
 const FlagCard = () => {
-  return <div>Flags</div>;
+  const [flag, setFlag] = useState([]);
+
+  const fetchFlags = async () => {
+    let url = "https://restcountries.com/v3.1/all";
+    try {
+        let response = await axios(url);
+        if (response.status === 200) {
+          setFlag(response.data);
+        }
+    //   setFlag(data);
+    } catch (error) {
+      console.error("Error Message: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFlags();
+  }, []);
+
+  console.log(flag);
+
+  if (flag.length === 0) {
+    return (
+      <div>
+        <img src={walk} alt="Loader.jpg" width={100} />
+        <p>Loading...</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {flag.map((country) => {
+        return <CountryCard country={country} />;
+      })}
+    </div>
+  );
 };
 
 export default FlagCard;
